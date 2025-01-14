744d3aa2bcb8cb867b58cfad5a092f58eef11a8b
Raja Shekar <rajashekarb.dev@gmail.com>
2025-01-14 21:24:32 +0000

    Added: entry point file created

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import logging
+
+# Configurations
+logging.basicConfig(format="%(levelname)s: %(message)s: %(asctime)s", 
+                    level=logging.DEBUG,
+                    datefmt="%Y-%m-%d %H:%M")
+
+logging.info("AlertOps AI Application Started Successfully")
